7797a297023ddc18a99842085e94fbb180b1f0a4
davinahouquet <davina.houquet@hotmail.com>
2023-05-02 09:10:09 +0200

    Données personne

---
+++ requete SQL.sql
@@ -0,0 +1,19 @@
+-- Requêtes pour remplir la base de données--
+
+--personne : réalisateur/trices
+INSERT INTO personne VALUES('1', 'Kubrick', 'Stanley', 'Homme', '1928-07-26')
+INSERT INTO personne VALUES('2', 'King', 'Stephen', 'Homme', '1947-09-21')
+INSERT INTO personne VALUES('3', 'Wan', 'James', 'Homme', '1977-02-26')
+INSERT INTO personne VALUES('4', 'Ducourneau', 'Julia', 'Femme', '1983-11-18')
+INSERT INTO personne VALUES('5', 'Watts', 'Jon', 'Homme', '1981-06-28')
+INSERT INTO personne VALUES('6', 'Jenkins', 'Patty', 'Femme', '1971-07-24')
+INSERT INTO personne VALUES('7', 'Coppola', 'Sofia', 'Femme', '1971-05-14')
+
+--personne : acteur/trices
+INSERT INTO personne VALUES('8', 'Murray', 'Bill', 'Homme', '2004-01-07')
+INSERT INTO personne VALUES('9', 'Johansson', 'Scarlett', 'Femme', '1984-11-22')
+INSERT INTO personne VALUES('10', 'Gadot', 'Gal', 'Femme', '1985-04-30')
+INSERT INTO personne VALUES('11', 'Skarsgard', 'Bill', 'Homme', '1990-08-09')
+INSERT INTO personne VALUES('12', 'Martell', 'Jaeden', 'Homme', '2003-01-04')
+INSERT INTO personne VALUES('13', 'Nicholson', 'Jack', 'Homme', '1937-04-22')
+INSERT INTO personne VALUES('14', 'Duvall', 'Shelley', 'Femme', '1979-07-07')
