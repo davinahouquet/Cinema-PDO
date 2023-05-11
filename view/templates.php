539790e5a06189176c00ea14b88128bdad79e1e6
davinahouquet <davina.houquet@hotmail.com>
2023-05-11 10:06:46 +0200

    Templates.php - initialisation

---
+++ view/templates.php
@@ -0,0 +1,32 @@
+<?php
+// Template will include fixed elements (like nav for ex.). We'll only have to link css or fonts once.
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/public/css/style.css">
+    <title>Cinema-MVC</title>
+</head>
+<body>
+    <h1>hello world</h1>
+    <section>
+        <!-- Nav -->
+        <nav>
+            <img src="/public/img/Logo.png">
+            <p>Mooviz</p>
+            <ul>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+        </nav>
+    </section>
+    <script src="/public/js/script.js"></script>
+</body>
+</html>
